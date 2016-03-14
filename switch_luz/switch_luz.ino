c85bc71ce243cc81aff79d7b83350115275bb6ec
Gabriel Fiocchi <fiocchigabriel@gmail.com>
2016-03-13 23:16:37 -0300

    Add multiple turn ON/OFF led's

--- switch_luz/switch_luz.ino
+++ switch_luz/switch_luz.ino
@@ -1,38 +1,54 @@
-int inPin = 2;         // the number of the input pin
-int outPin = 8;       // the number of the output pin
 
-int state = LOW;      // the current state of the output pin
-int reading;           // the current reading from the input pin
-int previous = HIGH;    // the previous reading from the input pin
+int inPin = 0;
+int outPin = 1;
+int state = 2;
+int reading = 3;
+int previous = 4;
+
+int pinMatrix[6][5] = {
+ {2, 8, LOW, 0, HIGH},
+ {3, 9, LOW, 0, HIGH},
+ {4, 10, LOW, 0, HIGH},
+ {5, 11, LOW, 0, HIGH},
+ {6, 12, LOW, 0, HIGH},
+ {7, 13, LOW, 0, HIGH}
+};
 
 // the follow variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
-long time = 0;         // the last time the output pin was toggled
-long debounce = 200;   // the debounce time, increase if the output flickers
+long time = 0;          // the last time the output pin was toggled
+long debounce = 200;    // the debounce time, increase if the output flickers
 
 void setup()
 {
-  pinMode(inPin, INPUT);
-  pinMode(outPin, OUTPUT);
+ // pinMode(inPin, INPUT);
+ // pinMode(outPin, OUTPUT);
+ for (int i = 0; i < 6; i++) {
+   pinMode(pinMatrix[i][inPin], INPUT);
+   pinMode(pinMatrix[i][outPin], OUTPUT);
+ }
 }
 
 void loop()
 {
-  reading = digitalRead(inPin);
+ for (int j = 0; j < 6; j++) {
+   pinMatrix[j][reading] = digitalRead(pinMatrix[j][inPin]);
 
-  // if the input just went from LOW and HIGH and we've waited long enough
-  // to ignore any noise on the circuit, toggle the output pin and remember
-  // the time
-  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
-    if (state == HIGH)
-      state = LOW;
-    else
-      state = HIGH;
+   // if the input just went from LOW and HIGH and we've waited long enough
+   // to ignore any noise on the circuit, toggle the output pin and remember
+   // the time
+   if (pinMatrix[j][reading] == HIGH && pinMatrix[j][previous] == LOW && millis() - time > debounce) {
+     if (pinMatrix[j][state] == HIGH)
+       pinMatrix[j][state] = LOW;
+     else
+       pinMatrix[j][state] = HIGH;
 
-    time = millis();    
-  }
+     time = millis();
+   }
 
-  digitalWrite(outPin, state);
+   digitalWrite(pinMatrix[j][outPin], pinMatrix[j][state]);
 
-  previous = reading;
+   pinMatrix[j][previous] = pinMatrix[j][reading];
+
+ }
 }
